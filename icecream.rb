b4a7fe29d8ec94ecdbb9fc142a424533d31e19d4
Sean Kim and Ryan Deussing <seanwoojinkim@gmail.com>
2013-01-17 09:46:46 -0800

    first commit

---
+++ icecream.rb
@@ -0,0 +1,6 @@
+# google_api_key = AIzaSyAUvCsOej1DYdGMS_g7DFRv7au5LbNnkGg
+require 'rest-client'
+
+# identify our current location
+# find nearby ice cream shops
+# for each ice cream shop, generate walking directions between us and the shop
