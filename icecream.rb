7bce7b67a863ec8f57db36e1d157802936342575
Kyle and Adam <kyle@lucovsky.com>
2013-01-17 11:17:55 -0800

    accessed nearby stores and print them for user

--- icecream.rb
+++ icecream.rb
@@ -5,24 +5,34 @@ require './secret_keys.rb'
 
 class IceCreamFinder
 
-  def run
-    get_key
-    location_coordinates
-    nearby_shops
+  def initialize
+    @google_key = Keys.new.get_google_key
   end
 
-  def get_user_location
+  def run
+    location_coordinates
+    print_stores
+  end
+
+  def user_location
     puts "What address you at?"
     print "> "
     gets.chomp
   end
 
-  def get_key
-    @google_key = Keys.new.get_google_key
+  def print_stores
+    stores = process_nearby_stores
+    stores.each do |store|
+      puts "Name: #{store[:name]}"
+      puts "Rating: #{store[:rating]}"
+      puts "Open: #{store[:open]}"
+      puts "-" * 8
+    end
+    nil
   end
 
   def location_coordinates
-    location = get_user_location
+    location = user_location
     url = Addressable::URI.new(
         :scheme => 'http',
         :host => 'maps.googleapis.com',
@@ -33,7 +43,7 @@ class IceCreamFinder
     @user_coord = response["results"][0]["geometry"]["location"]
   end
 
-  def nearby_shops
+  def nearby_stores
     location = "#{@user_coord["lat"]},#{@user_coord["lng"]}"
     url = Addressable::URI.new(
         :scheme => 'https',
@@ -48,7 +58,20 @@ class IceCreamFinder
         }
       ).to_s
     response = JSON.parse(RestClient.get(url))
-    p response
+  end
+
+  def process_nearby_stores
+    stores = nearby_stores
+    store_array = []
+    stores['results'].each do |store|
+      store_hash = {}
+      store_hash[:location] = store['geometry']['location']
+      store_hash[:name] = store['name']
+      store_hash[:open] = store['opening_hours']['open_now']
+      store_hash[:rating] = store['rating']
+      store_array << store_hash
+    end
+    store_array
   end
 
   def directions
@@ -56,3 +79,9 @@ class IceCreamFinder
   end
 
 end
+
+
+#location_hash = response["results"][0]['geometry']['location']
+#name = response["results"][0]['name']
+#open = response["results"][0]['opening_hours']['open_now']
+#rating = response["results"][0]['rating']
